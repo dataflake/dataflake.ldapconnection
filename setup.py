ae941bbdff43926f3a0e7f19841812b83f985918
Chris McDonough <chrism@plope.com>
2008-06-06 17:24:13 +0000

    Initial (non-functional) import.
    git-svn-id: file:///svn-public/dataflake.ldapconnection@1519 835909ba-7c00-0410-bfa4-884f43845301

---
+++ setup.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2000-2008 Jens Vagelpohl and Contributors. All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+__version__ = '0.1'
+
+import os
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+setup(name='dataflake.ldapconnection',
+      version=__version__,
+      description='LDAP connection library',
+      long_description=README,
+      classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        ],
+      keywords='ldap',
+      author="Agendaless Consulting and Jens Vagelpohl",
+      url="http://www.dataflake.org/software/dataflake.ldapconnection",
+      license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['dataflake'],
+      zip_safe=False,
+      tests_require = [
+              'python-ldap',
+              ],
+      install_requires=[
+              'python-ldap'
+              ],
+      test_suite='dataflake.ldapconnection.tests',
+      )
+
