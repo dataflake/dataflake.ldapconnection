f185ba86db6994b366ebce3244dd1186a280ae60
Jens Vagelpohl <jens@netz.ooo>
2017-10-29 12:45:12 +0100

    - correct module name

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['pyldap']
+MOCK_MODULES = ['ldap']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
